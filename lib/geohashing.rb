b3475dd69df96eedaf7e7e97a810e53a142446fe
Harry Schwartz <harryrschwartz@gmail.com>
2012-02-28 14:22:34 -0500

    Fixing hex_to_dec bug, using better DJI source.

--- lib/geohashing.rb
+++ lib/geohashing.rb
@@ -1,13 +1,12 @@
 require 'net/http'
-require 'rexml/document'
 require 'digest'
 
 module Geohashing
+  # using the peeron API
+  # see: http://wiki.xkcd.com/geohashing/Dow_Jones_Industrial_Average
   def last_dow_open
-    dow_query = "http://www.google.com/ig/api?stock=.DJI"
-    doc_data = Net::HTTP.get_response(URI.parse(dow_query)).body
-    doc = REXML::Document.new doc_data
-    doc.elements["xml_api_reply/finance/open"].attributes["data"].to_f
+    url = Time.new.strftime("http://carabiner.peeron.com/xkcd/map/data/%Y/%m/%d")
+    Net::HTTP.get_response(URI.parse(url)).body.to_f
   end
 
   def today
@@ -26,16 +25,30 @@ module Geohashing
     [sum[0..15], sum[16..31]]
   end
 
+  # this method slightly modified from ScottKuma's
+  # "Ruby Geohasher v1.2" implementation
+  # http://wiki.xkcd.com/geohashing/Implementations#Ruby_Geohasher_v1.2
   def hex_to_dec h
-    h.to_i(16).to_s
+    fracLen = h.length
+    fracPortion = h[0..(fracLen-1)]
+    fracLen = fracPortion.length
+    len = (fracLen - 1)
+    sum = 0
+    (0..len).each do |i|
+      numSixteenths = fracPortion[i..i].to_i(16)
+      conversionFactor = (16.**(i + 1)).to_f
+      conversionFactor = 1./conversionFactor
+      sum = sum + ((numSixteenths) * conversionFactor)
+    end
+    return sum.to_s[2..8]
   end
 
-  def base_position pos
+  def graticule pos
     pos < 0 ? pos.ceil : pos.floor
   end 
 
   def geohash lat, long
-    lat, long = base_position(lat), base_position(long)
+    lat, long = graticule(lat), graticule(long)
     lat_dec, long_dec = md5split(md5sum(code_today)).map{ |s| hex_to_dec s }
     ["#{lat}.#{lat_dec}".to_f, "#{long}.#{long_dec}".to_f]
   end
